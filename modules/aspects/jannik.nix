25e63adc469b41c6b22b1e8d9aa4a19ce96db95c
Jannik Zenker <accounts@jannikzenker.de>
2026-06-13 23:04:44 +0200

    Fix aspect import for jannik
    - fix: fixed missing import for ghostty aspect for user jannik

--- modules/aspects/jannik.nix
+++ modules/aspects/jannik.nix
@@ -4,6 +4,7 @@
   den.aspects.jannik = {
     includes = [
       den.aspects.fonts
+      den.aspects.ghostty
       den.aspects.niri
       den.batteries.primary-user
       (den.batteries.user-shell "zsh")
