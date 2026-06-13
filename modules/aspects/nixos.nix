9f4adeeb66a0610801f0b2aacc3dccb3934e5b99
Jannik Zenker <accounts@jannikzenker.de>
2026-06-13 19:21:25 +0200

    Add locale and timezone settings to defaults
    - feat: add locale and timezone settings to module "nixos"

--- modules/aspects/nixos.nix
+++ modules/aspects/nixos.nix
@@ -1,5 +1,5 @@
 {
-  den.aspects.nixos = {
+  den.aspects.nixos = { host, ... }: {
     nixos = {
       nix = {
         # Enable flake functionality and new CLI
@@ -20,6 +20,28 @@
         };
       };
       systemd.timers.nix-optimise.timerConfig.Persistent = true; # If host was offline during optimize time -> do it on next boot
+
+      # locale settings
+      i18n = {
+        defaultLocale = "de_DE.UTF-8";
+        extraLocaleSettings = {
+          LC_ADDRESS = "de_DE.UTF-8";
+          LC_COLLATE = "de_DE.UTF-8";
+          LC_CTYPE = "de_DE.UTF-8";
+          LC_IDENTIFICATION = "de_DE.UTF-8";
+          LC_MONETARY = "de_DE.UTF-8";
+          LC_MESSAGES = "en_GB.UTF-8";
+          LC_MEASUREMENT = "de_DE.UTF-8";
+          LC_NAME = "de_DE.UTF-8";
+          LC_NUMERIC = "de_DE.UTF-8";
+          LC_PAPER = "de_DE.UTF-8";
+          LC_TELEPHONE = "de_DE.UTF-8";
+          LC_TIME = "de_DE.UTF-8";
+        };
+      };
+
+      # Take local time for desktops, UTC for servers and as a fallback
+      time.timeZone = if host.profile == "desktop" then "Europe/Berlin" else "UTC";
     };
   };
 }
