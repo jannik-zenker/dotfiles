d018e13046e29c94e2bc48a5e311c212d468cc38
Jannik Zenker <accounts@jannikzenker.de>
2026-06-14 15:35:13 +0200

    Add automatic installation of firmware and uefi updates
    - feat: add automatic installation of needed firmware for every host
    (includes firmware from unfree repos)
    - feat: add automatic uefi update service
    - config: allow unfree packages by default for firmware installation

--- modules/defaults.nix
+++ modules/defaults.nix
@@ -1,26 +1,36 @@
 # Contains global settings across all hosts, users and standalone homes
 { lib, den, ... }:
 {
-  den.schema = { host, ... }: {
-    host = {
-      # Set hostnames from host declarations, install bootloader and the disko module
-      includes = [
-        den.aspects.bootloader
-        den.aspects.defaultPackages
-        den.aspects.disko
-        den.aspects.git
-        den.aspects.graphics
-        den.aspects.journald
-        den.aspects.nixos
-        den.batteries.hostname
-      ];
+  den = {
+    default = {
+      nixos = {
+        nixpkgs.config.allowUnfree = true; # needed for proprietary firmware
+        hardware.enableAllFirmware = true;
+        services.fwupd.enable = true;
+      };
     };
 
-    user = {
-      # Enable home-manager class evaluation by default for every user
-      classes = lib.mkDefault [ "homeManager" ];
-      # Create users from user declarations in host declarations
-      includes = with den.batteries; [ define-user ];
+    schema = {
+      host = {
+        # Set hostnames from host declarations, install bootloader and the disko module
+        includes = [
+          den.aspects.bootloader
+          den.aspects.defaultPackages
+          den.aspects.disko
+          den.aspects.git
+          den.aspects.graphics
+          den.aspects.journald
+          den.aspects.nixos
+          den.batteries.hostname
+        ];
+      };
+
+      user = {
+        # Enable home-manager class evaluation by default for every user
+        classes = lib.mkDefault [ "homeManager" ];
+        # Create users from user declarations in host declarations
+        includes = with den.batteries; [ define-user ];
+      };
     };
   };
 }
