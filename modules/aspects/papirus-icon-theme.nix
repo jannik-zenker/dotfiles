e1d7793e38ce67d61cda68e4f6632ab59551a401
Jannik Zenker <accounts@jannikzenker.de>
2026-06-15 16:57:45 +0200

    Switched to regular installation of papirus icon theme

--- modules/aspects/papirus-icon-theme.nix
+++ modules/aspects/papirus-icon-theme.nix
@@ -6,21 +6,7 @@
     };
 
     homeManager = { lib, pkgs, ... }: {
-      # This Method installs papirus-icon-theme MUTABLE for theme switchers to work
-      # with papirus-icon-themes
-      home.activation.copyPapirusIcons = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        _target="$HOME/.local/share/icons/Papirus"
-        _version="${pkgs.papirus-icon-theme.version}"
-        _marker="$_target/.nix-version"
-
-        if [[ "$(cat "$_marker" 2>/dev/null)" != "$_version" ]]; then
-          rm -rf "$_target"
-          mkdir -p "$HOME/.local/share/icons"
-          cp -r "${pkgs.papirus-icon-theme}/share/icons/Papirus" "$_target"
-          chmod -R u+w "$_target"
-          echo "$_version" > "$_marker"
-        fi
-      '';
+      home.packages = with pkgs; [ papirus-icon-theme ];
     };
   };
 }
