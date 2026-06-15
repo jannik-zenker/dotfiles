55cd6f57969d35934c60cdcbf2a3eca9663dcc92
Jannik Zenker <accounts@jannikzenker.de>
2026-06-15 23:06:09 +0200

    Removed invalid pywalfox flag

--- modules/aspects/pywalfox.nix
+++ modules/aspects/pywalfox.nix
@@ -3,7 +3,6 @@
     homeManager = { pkgs, lib, ... }: {
       home.activation.pywalfox = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
         ${pkgs.pywalfox-native}/bin/pywalfox install
-        ${pkgs.pywalfox-native}/bin/pywalfox install --thunderbird
       '';
     };
   };
