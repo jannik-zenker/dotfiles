41b98c382b1e0947d9dade72f58839af58b69f9e
Jannik Zenker <accounts@jannikzenker.de>
2026-06-15 10:33:13 +0200

    Remove QT theming env var from niri config
    - config: removed QT theming env var from niri config since it will be
    managed by home-manager in the future

--- modules/aspects/niri/config/configuration/environment.nix
+++ modules/aspects/niri/config/configuration/environment.nix
@@ -1,12 +1,7 @@
 {
   den.aspects.niri = {
     homeManager = {
-      xdg.configFile."niri/configuration/environment.kdl".text = ''
-        environment {
-            QT_QPA_PLATFORM "wayland"
-            QT_QPA_PLATFORMTHEME "gtk3"
-        }
-      '';
+      xdg.configFile."niri/configuration/environment.kdl".text = "";
     };
   };
 }
