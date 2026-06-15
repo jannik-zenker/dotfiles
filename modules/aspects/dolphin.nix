7c65cb6c613e4220c4623e5eb2dad22666132651
Jannik Zenker <accounts@jannikzenker.de>
2026-06-15 10:43:45 +0200

    Fixed package name in aspect "dolphin"

--- modules/aspects/dolphin.nix
+++ modules/aspects/dolphin.nix
@@ -1,7 +1,7 @@
 {
   den.aspects.dolphin = {
     homeManager = { pkgs, ... }: {
-      home.packages = with pkgs; [ dolphin ];
+      home.packages = with pkgs; [ kdePackages.dolphin ];
     };
   };
 }
