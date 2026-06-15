e033ab22b94ace9ba107d490aa26a47878b6f255
Jannik Zenker <accounts@jannikzenker.de>
2026-06-15 22:53:03 +0200

    Fix cava
    - fix: fixed lib import in wrong scope

--- modules/aspects/cava.nix
+++ modules/aspects/cava.nix
@@ -1,17 +1,23 @@
-{ lib, ... }:
 {
   den.aspects.cava = {
-    homeManager = { config, pkgs, ... }: {
-      home.activation.cavaConfig = lib.hm.dag.entryBefore [ "writeBoundary" ] ''
-        rm -f "${config.xdg.configHome}/cava/config"
-      '';
+    homeManager =
+      {
+        config,
+        lib,
+        pkgs,
+        ...
+      }:
+      {
+        home.activation.cavaConfig = lib.hm.dag.entryBefore [ "writeBoundary" ] ''
+          rm -f "${config.xdg.configHome}/cava/config"
+        '';
 
-      programs.cava = {
-        enable = true;
-        settings = {
-          color.theme = lib.mkIf (lib.attrByPath [ "programs" "noctalia" "enable" ] false config) "noctalia";
+        programs.cava = {
+          enable = true;
+          settings = {
+            color.theme = lib.mkIf (lib.attrByPath [ "programs" "noctalia" "enable" ] false config) "noctalia";
+          };
         };
       };
-    };
   };
 }
