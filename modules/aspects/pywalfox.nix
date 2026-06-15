c0d5ee2fea4ceebfffad14b4b539bad366af1ed2
Jannik Zenker <accounts@jannikzenker.de>
2026-06-15 23:15:47 +0200

    Remove unecessary pywalfox symlink

--- modules/aspects/pywalfox.nix
+++ modules/aspects/pywalfox.nix
@@ -3,9 +3,6 @@
     homeManager = { pkgs, lib, ... }: {
       home.activation.pywalfox = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
         ${pkgs.pywalfox-native}/bin/pywalfox install
-        mkdir -p "$HOME/.thunderbird/native-messaging-hosts"
-        ln -sf "$HOME/.mozilla/native-messaging-hosts/pywalfox.json" \
-               "$HOME/.thunderbird/native-messaging-hosts/pywalfox.json"
       '';
     };
   };
