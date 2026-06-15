2de80f187620b851afd4dc3fb8d00295ca71daee
Jannik Zenker <accounts@jannikzenker.de>
2026-06-15 12:05:49 +0200

    Fix qt aspect

--- modules/aspects/qt.nix
+++ modules/aspects/qt.nix
@@ -7,17 +7,10 @@
         platformTheme.name = "qtct";
       };
 
-      home.packages = [
-        (pkgs.kdePackages.qt6ct.overrideAttrs (old: {
-          patches = (old.patches or [ ]) ++ [ ./qt6ct-0.11.patch ];
-        }))
-      ];
-
       home.sessionVariables.QT_QPA_PLATFORMTHEME = lib.mkForce "qt6ct";
 
       home.file.".config/qt6ct/qt6ct.conf".text = ''
         [Appearance]
-        style=Fusion
         icon_theme=Papirus-Dark
         custom_palette=true
         color_scheme_path=${config.home.homeDirectory}/.config/qt6ct/colors/noctalia.conf
