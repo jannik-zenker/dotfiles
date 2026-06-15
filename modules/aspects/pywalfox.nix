f67b7dd3deec7d552a49fa6798c933f4b13ed7ef
Jannik Zenker <accounts@jannikzenker.de>
2026-06-15 23:02:02 +0200

    Add thunderbird support for pywalfox

--- modules/aspects/pywalfox.nix
+++ modules/aspects/pywalfox.nix
@@ -3,6 +3,7 @@
     homeManager = { pkgs, lib, ... }: {
       home.activation.pywalfox = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
         ${pkgs.pywalfox-native}/bin/pywalfox install
+        ${pkgs.pywalfox-native}/bin/pywalfox install --thunderbird
       '';
     };
   };
