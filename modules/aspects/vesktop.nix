3f5fdcddd11aac05bee3f3e35a6999ec8a580a49
Jannik Zenker <accounts@jannikzenker.de>
2026-06-15 23:54:50 +0200

    Add vesktop settings
    - config: vesktop automatically enables noctalia theme
    - config: vesktop automatically enables fake-nitro

--- modules/aspects/vesktop.nix
+++ modules/aspects/vesktop.nix
@@ -3,6 +3,12 @@
     homeManager = {
       programs.vesktop = {
         enable = true;
+        vencord.settings = {
+          enabledThemes = [ "noctalia-material.theme.css" ];
+          plugins = {
+            FakeNitro.enabled = true;
+          };
+        };
       };
     };
   };
