b22f6fec4208b80f6ef3907dbfbe6d34311ca419
Jannik Zenker <accounts@jannikzenker.de>
2026-06-15 11:41:48 +0200

    Add noctalia theme and papirus folders to qt aspect

--- modules/aspects/qt.nix
+++ modules/aspects/qt.nix
@@ -1,14 +1,25 @@
-{ lib, ... }:
 {
-  den.aspects.qt = {
-    homeManager = { ... }: {
+  den.aspects.qtTheming = {
+    homeManager = { config, ... }: {
       qt = {
         enable = true;
+        platformTheme.name = "qtct";
       };
 
-      home.sessionVariables = {
-        QT_QPA_PLATFORMTHEME = lib.mkDefault "gtk3";
-      };
+      home.sessionVariables.QT_QPA_PLATFORMTHEME = "qt6ct";
+
+      home.file.".config/qt6ct/qt6ct.conf".text = ''
+        [Appearance]
+        style=Fusion
+        icon_theme=Papirus-Dark
+        custom_palette=true
+        color_scheme_path=${config.home.homeDirectory}/.config/qt6ct/colors/noctalia.conf
+      '';
+
+      home.file.".config/kdeglobals".text = ''
+        [Icons]
+        Theme=Papirus-Dark
+      '';
     };
   };
 }
