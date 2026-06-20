4e08b4f9864235ec8f2885267db4223ce41b2242
Jannik Zenker <accounts@jannikzenker.de>
2026-06-20 22:46:54 +0200

    Add mist settings for niri
    - feat: add config file for niri misc settings

---
+++ modules/aspects/niri/config/misc.nix
@@ -0,0 +1,15 @@
+{
+  den.aspects.niri = {
+    homeManager = {
+      programs.niri.settings = {
+        hotkey-overlay = {
+          skip-at-startup = true;
+          hide-not-bound = true;
+        };
+        prefer-no-csd = true;
+        screenshot-path = "~/Pictures/Screenshots/scrsh_%Y-%m-%d %H-%M-%S.png";
+        clipboard.disable-primary = true;
+      };
+    };
+  };
+}
