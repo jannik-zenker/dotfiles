99c530a40e4e8f9cf9115b881d452e44d9684573
Jannik Zenker <accounts@jannikzenker.de>
2026-06-20 14:06:54 +0200

    Enable dconf on system if using gtk aspect

--- modules/aspects/gtk.nix
+++ modules/aspects/gtk.nix
@@ -1,11 +1,12 @@
 {
   den.aspects.gtk = {
-    nixos = { pkgs, ... }: {
+    provides.to-hosts.nixos = { pkgs, ... }: {
 
       # Fix gtk3 apps not finding schemas
       environment.sessionVariables.XDG_DATA_DIRS = [
         "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
       ];
+      programs.dconf.enable = true;
     };
 
     homeManager = { pkgs, ... }: {
