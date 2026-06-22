ea229b8af9c0c4f030524b39d7e13a91a26b9315
Jannik Zenker <accounts@jannikzenker.de>
2026-06-22 11:37:33 +0200

    Add monitor and workspace settings (niri) for host "sirene"

--- modules/aspects/niri/config/configuration/monitors.nix
+++ modules/aspects/niri/config/configuration/monitors.nix
@@ -1,19 +1,37 @@
 {
-  den.aspects.niri = {
+  den.aspects.niri = { host, ... }: {
     homeManager = {
-      xdg.configFile."niri/configuration/monitors.kdl".text = ''
-        output "ASUSTek COMPUTER INC VG27A R7LMQS113801" {
-            mode "2560x1440@164.999"
-            scale 1
-            position x=1920 y=0
-            focus-at-startup
-        }
-        output "LG Electronics 24GM79G 0x0008040D" {
-            mode "1920x1080@144.001"
-            scale 1
-            position x=0 y=150
-        }
-      '';
+      xdg.configFile."niri/configuration/monitors.kdl".text =
+        if host.name == "reacher" then
+          ''
+            output "ASUSTek COMPUTER INC VG27A R7LMQS113801" {
+                mode "2560x1440@164.999"
+                scale 1
+                position x=1920 y=0
+                focus-at-startup
+            }
+            output "LG Electronics 24GM79G 0x0008040D" {
+                mode "1920x1080@144.001"
+                scale 1
+                position x=0 y=150
+            }
+          ''
+        else if host.name == "sirene" then
+          ''
+            output "Dell Inc. DELL P2725D DB7HRC4" {
+                mode "2560x1440@99.946"
+                scale 1
+                position x=1920 y=0
+                focus-at-startup
+            }
+            output "Dell Inc. DELL P2725HE BY7J584" {
+                mode "1920x1080@100.000"
+                scale 1
+                position x=0 y=150
+            }
+          ''
+        else
+          "";
     };
   };
 }
--- modules/aspects/niri/config/configuration/workspaces.nix
+++ modules/aspects/niri/config/configuration/workspaces.nix
@@ -1,24 +1,51 @@
 {
-  den.aspects.niri = {
+  den.aspects.niri = { host, ... }: {
     homeManager = {
       xdg.configFile."niri/configuration/workspaces.kdl".text = ''
         workspace "terminal"
         workspace "work"
         workspace "code"
         workspace "gaming"
-        workspace "browser" {
-            open-on-output "LG Electronics 24GM79G 0x0008040D"
-        }
-        workspace "chat" {
-            open-on-output "LG Electronics 24GM79G 0x0008040D"
-        }
-        workspace "mail" {
-            open-on-output "LG Electronics 24GM79G 0x0008040D"
-        }
-        workspace "music" {
-            open-on-output "LG Electronics 24GM79G 0x0008040D"
-        }
-      '';
+      ''
+      + (
+        if (host.name == "reacher") then
+          ''
+            workspace "browser" {
+                open-on-output "LG Electronics 24GM79G 0x0008040D"
+            }
+            workspace "chat" {
+                open-on-output "LG Electronics 24GM79G 0x0008040D"
+            }
+            workspace "mail" {
+                open-on-output "LG Electronics 24GM79G 0x0008040D"
+            }
+            workspace "music" {
+                open-on-output "LG Electronics 24GM79G 0x0008040D"
+            }
+          ''
+        else if (host.name == "sirene") then
+          ''
+            workspace "browser" {
+                open-on-output "Dell Inc. DELL P2725D DB7HRC4"
+            }
+            workspace "chat" {
+                open-on-output "Dell Inc. DELL P2725D DB7HRC4"
+            }
+            workspace "mail" {
+                open-on-output "Dell Inc. DELL P2725D DB7HRC4"
+            }
+            workspace "music" {
+                open-on-output "Dell Inc. DELL P2725D DB7HRC4"
+            }
+          ''
+        else
+          ''
+            workspace "browser"
+            workspace "chat"
+            workspace "mail"
+            workspace "music"
+          ''
+      );
     };
   };
 }
