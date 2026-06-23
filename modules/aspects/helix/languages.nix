8b665c966a05955f9bf34fa6144e1e7b0cc0f79b
Jannik Zenker <accounts@jannikzenker.de>
2026-06-23 22:48:43 +0200

    Add latex language support to helix config
    - feat: add latex language support for aspect "helix" including lsp,
    formatting and auto compile

--- modules/aspects/helix/languages.nix
+++ modules/aspects/helix/languages.nix
@@ -6,6 +6,20 @@
           language-server.nixd = {
             command = "${pkgs.nixd}/bin/nixd";
           };
+          language-server.texlab = {
+            command = "${pkgs.texlab}/bin/texlab";
+            config.texlab.build = {
+              onSave = true;
+              executable = "latexmk";
+              args = [
+                "-lualatex"
+                "-interaction=nonstopmode"
+                "-synctex=1"
+                "-auxdir=build"
+                "%f"
+              ];
+            };
+          };
 
           language = [
             {
@@ -14,6 +28,11 @@
               formatter = {
                 command = "${pkgs.nixfmt}/bin/nixfmt";
               };
+              auto-format = true;
+            }
+            {
+              name = "latex";
+              language-servers = [ "texlab" ];
               auto-format = true;
             }
           ];
