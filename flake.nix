52aade02cd757a2f9c8c67ef347f1d532c9668a9
Jannik Zenker <accounts@jannikzenker.de>
2026-06-23 15:44:53 +0200

    Remove extra substituters from flake config
    - config: remove extra substituters for noctalia from flake config since
    they are embedded into nixos config

--- flake.nix
+++ flake.nix
@@ -3,13 +3,6 @@
 {
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 
-  nixConfig = {
-    extra-substituters = [ "https://noctalia.cachix.org" ];
-    extra-trusted-public-keys = [
-      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
-    ];
-  };
-
   inputs = {
     den.url = "github:denful/den";
     disko = {
