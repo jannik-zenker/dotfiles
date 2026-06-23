a3ba04b0dfd62ce41ebdea981677823bd159a574
Jannik Zenker <accounts@jannikzenker.de>
2026-06-23 21:31:57 +0200

    Use xdg dirs for texlive
    - config: aspect "texlive" now sets environment variables to use
    xdg-directories for texlive  when using homeManager

--- modules/aspects/texlive.nix
+++ modules/aspects/texlive.nix
@@ -3,5 +3,13 @@
     nixos = { pkgs, ... }: {
       environment.systemPackages = with pkgs; [ texliveFull ];
     };
+
+    provides.to-users.homeManager = { config, ... }: {
+      home.sessionVariables = {
+        TEXMFHOME = "${config.xdg.dataHome or "$HOME/.local/share"}/texmf";
+        TEXMFVAR = "${config.xdg.cacheHome or "$HOME/.cache"}/texmf";
+        TEXMFCONFIG = "${config.xdg.configHome or "$HOME/.config"}/texmf";
+      };
+    };
   };
 }
