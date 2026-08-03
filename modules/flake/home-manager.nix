86d630e820755fe7e3982a8d53ca2c7edc81054b
Jannik Zenker <accounts@jannikzenker.de>
2026-08-03 10:45:58 +0200

    Add new password file for lumiere and converted password files for new admin key

--- modules/flake/home-manager.nix
+++ modules/flake/home-manager.nix
@@ -1,10 +1,12 @@
 {
-  home-manager = {
-    url = "github:nix-community/home-manager";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
-  home-manager-stable = {
-    url = "github:nix-community/home-manager/release-26.05";
-    inputs.nixpkgs.follows = "nixpkgs-stable";
+  flake-file.inputs = {
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager-stable = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
+    };
   };
 }
