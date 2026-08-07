313800ea97e852287aa5de87b1edc011ab648362
Jannik Zenker <accounts@jannikzenker.de>
2026-08-07 17:00:26 +0200

    Add dns over tls config to adguardHome

--- modules/aspects/adguard-home.nix
+++ modules/aspects/adguard-home.nix
@@ -26,10 +26,7 @@
             ];
 
             upstream_dns = [
-              "9.9.9.9#dns.quad9.net"
-              "149.112.112.112#dns.quad9.net"
-              "2620:fe::fe#dns.quad9.net"
-              "2620:fe::9#dns.quad9.net"
+              "tls://dns.quad9.net"
             ];
           };
 
