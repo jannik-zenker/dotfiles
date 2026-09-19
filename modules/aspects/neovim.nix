5b8f4e71c3833be1b5aab6882634cae5677276a2
Jannik Zenker <accounts@jannikzenker.de>
2026-09-19 16:38:14 +0200

    feat: register nvim as default editor
    Declares defaultApps.editor so the xdg/environment aspects pick nvim
    as the default text editor.

--- modules/aspects/neovim.nix
+++ modules/aspects/neovim.nix
@@ -1,5 +1,10 @@
 { self, ... }: {
   den.aspects.neovim.homeManager = { pkgs, ... }: {
+    defaultApps.editor = {
+      command = "nvim";
+      desktopFile = "nvim.desktop";
+    };
+
     home.packages = [ self.packages.${pkgs.stdenv.hostPlatform.system}.neovim ];
   };
 }
